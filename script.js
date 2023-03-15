c47cdac6773d20fe8e57932e8085fd2851f74c98
IAN OTIENO <116643853+IanOrtis@users.noreply.github.com>
2023-03-15 10:48:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//Converts the string to an array of words using the .split() method
+let string = "you can share your opinion";
+let array = string.split(" ");
+console.log(array);
+
+//Sorts the array of words in alphabetical order using the .sort() method
+let sortedArray = array.sort();
+console.log(sortedArray);
+
+//Reverses the order of the array using the .reverse() method
+let reverseArray = sortedArray.reverse();
+console.log(reverseArray);
+
+//Extracts every second word from the reversed array using the .slice() method.
+const names = "you can share your opinion";
+const names2 = names.slice(18);
+console.log(names2);
